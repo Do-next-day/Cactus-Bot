7693261b98fea5f1377997fa05680cbc657b57fb
LaoLittle <y1941803875@outlook.com>
2022-03-01 13:56:55 +0800

    use user cookie

--- src/main/kotlin/CactusBot.kt
+++ src/main/kotlin/CactusBot.kt
@@ -18,6 +18,7 @@ import net.mamoe.mirai.utils.ExternalResource.Companion.toExternalResource
 import net.mamoe.mirai.utils.ExternalResource.Companion.uploadAsImage
 import net.mamoe.mirai.utils.info
 import org.jetbrains.skia.EncodedImageFormat
+import org.laolittle.plugin.genshin.api.ApiFailedAccessException
 import org.laolittle.plugin.genshin.api.bbs.BBSApi
 import org.laolittle.plugin.genshin.api.bbs.BBSData
 import org.laolittle.plugin.genshin.api.genshin.GenshinBBSApi
@@ -43,7 +44,6 @@ object CactusBot : KotlinPlugin(JvmPluginDescription(
         init()
         logger.info { "Plugin loaded" }
 
-
         globalEventChannel().subscribeGroupMessages {
             (startsWith("原神") or startsWith("派蒙")) Listener@{ foo ->
                 val result = Regex("""(人物|十连|单抽|查询|test)(.*)""").find(foo)?.groupValues
@@ -57,18 +57,24 @@ object CactusBot : KotlinPlugin(JvmPluginDescription(
                         users.remove(sender.id)
                     }
                     "查询" -> {
+                        val userData = cactusSuspendedTransaction {
+                            getUserData(sender.id)
+                        }
+
+                        val cookies = userData.data.cookies.takeIf { it.isNotBlank() } ?: CactusData.cookies
+
                         val uid = result[2].replace(Regex("""[\s]+"""), "").toLongOrNull()
                         if (uid == null || uid < 100000100 || uid > 700000000) {
                             subject.sendMessage("请输入正确的uid")
                             return@Listener
                         }
                         val query = kotlin.runCatching {
-                            GenshinBBSApi.getPlayerInfo(uid)
+                            GenshinBBSApi.getPlayerInfo(uid, cookies, userData.data.uuid)
                         }.getOrElse {
                             when (it) {
                                 is SerializationException -> subject.sendMessage("请求失败！请检查uid是否正确")
-                                is IllegalAccessException -> subject.sendMessage("获取失败: ${it.message}")
-                                else -> logger.info(it)
+                                is ApiFailedAccessException -> subject.sendMessage("获取失败: ${it.message}")
+                                else -> logger.error(it)
                             }
                             return@Listener
                         }
@@ -97,51 +103,62 @@ object CactusBot : KotlinPlugin(JvmPluginDescription(
         }
 
         globalEventChannel().subscribeFriendMessages {
-            startsWith("原神登录") Listener@{
+            "原神登录" Login@{
                 subject.sendMessage("请发送Cookie")
                 val cookies = nextMessageOrNull(30_000)?.content ?: kotlin.run {
                     subject.sendMessage("超时")
-                    return@Listener
+                    return@Login
                 }
 
                 val response = runCatching {
                     val roles = BBSApi.getRolesByCookie(cookies, BBSData.GameBiz.HK4E_CN)
+
                     if (roles.isEmpty()) {
-                        subject.sendMessage("没有找到绑定的角色！")
-                        return@Listener
+                        subject.sendMessage("没有找到绑定的角色!")
+                        return@Login
                     }
+
                     var role = roles.first()
                     cactusSuspendedTransaction {
                         val userData = getUserData(subject.id)
                         val data = userData.data
+                        // roles not empty
                         if (roles.size == 1) {
-                            data.setCookies(cookies)
+                            data.cookies = cookies
                         } else {
-                            var uid: Long
+                            subject.sendMessage(buildMessageChain {
+                                add("查询到以下信息: \n")
+                                roles.forEach { r ->
+                                    add("昵称: ${r.nickname}\nuid: ${r.gameUID}\n")
+                                }
+                                add("请发送uid进行绑定")
+                            })
                             nextMessageOrNull(30_000) { e ->
-                                uid = e.message.content.toLongOrNull() ?: return@nextMessageOrNull false
+                                val uid = e.message.content.toLongOrNull() ?: return@nextMessageOrNull false
                                 role = roles.firstOrNull { r -> r.gameUID == uid } ?: kotlin.run {
-                                    subject.sendMessage("超时")
                                     return@nextMessageOrNull false
                                 }
                                 true
-                            } ?: return@cactusSuspendedTransaction null
+                            } ?: kotlin.run {
+                                subject.sendMessage("超时")
+                                return@cactusSuspendedTransaction null
+                            }
 
-                            data.setCookies(cookies)
+                            data.cookies = cookies
                         }
 
                         userData.genshinUID = role.gameUID
                         userData.data = data
-                    } ?: return@Listener
+                    } ?: return@Login
                     role
                 }.getOrElse {
-                    subject.sendMessage("登录失败，原因: ${it.message}")
-                    return@Listener
+                    subject.sendMessage("登录失败, 原因: ${it.message}")
+                    return@Login
                 }
 
                 subject.sendMessage(
                     """
-                    旅行者${response.nickname}登录成功！
+                    旅行者${response.nickname}登录成功!
                     你的UID: ${response.gameUID}
                 """.trimIndent()
                 )
@@ -153,10 +170,10 @@ object CactusBot : KotlinPlugin(JvmPluginDescription(
                 val userData = cactusSuspendedTransaction {
                     getUserData(sender.id)
                 }
-                println(userData.data.toString())
-                val cookies = userData.data.cookieData.cookies
-                val uuid = userData.data.cookieData.uuid
-                if (cookies == null) {
+
+                val cookies = userData.data.cookies
+                val uuid = userData.data.uuid
+                if (cookies.isEmpty()) {
                     subject.sendMessage("请先登录")
                     return@Sign
                 }
@@ -169,10 +186,14 @@ object CactusBot : KotlinPlugin(JvmPluginDescription(
 
                 val region = role.region
 
-                val response = GenshinBBSApi.signGenshin(userData.genshinUID, region, cookies, uuid)
-                val message = if (response.isSuccess) "旅行者: ${role.nickname}签到成功" else response.message
+                kotlin.runCatching {
+                    GenshinBBSApi.signGenshin(userData.genshinUID, region, cookies, uuid)
+                }.onSuccess {
+                    subject.sendMessage("旅行者: ${role.nickname}签到成功")
+                }.onFailure {
+                    subject.sendMessage("签到失败: ${it.message}")
+                }
 
-                subject.sendMessage(message)
             }
         }
     }
