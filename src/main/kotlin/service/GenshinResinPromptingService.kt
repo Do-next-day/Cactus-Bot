333f0944085ad7519fe2691635b09a607be0fe86
LaoLittle <y1941803875@outlook.com>
2022-03-03 23:31:08 +0800

    use while

--- src/main/kotlin/service/GenshinResinPromptingService.kt
+++ src/main/kotlin/service/GenshinResinPromptingService.kt
@@ -12,28 +12,33 @@ import org.laolittle.plugin.genshin.util.requireCookie
 import org.laolittle.plugin.genshin.util.userSettings
 
 class GenshinResinPromptingService(private val user: User) : AbstractCactusService() {
+    private var repeat = 0
     override suspend fun main() {
-        val userData = user.requireCookie { return@requireCookie }
+        while (isActive) {
+            val userData = user.requireCookie { return@requireCookie }
 
-        val dailyNote = kotlin.runCatching {
-            userData.getDailyNote()
-        }.getOrElse {
-            logger.error { "用户$user 获取便笺数据失败, 已停止推送。原因: ${it.message}" }
-            return
+            val dailyNote = kotlin.runCatching {
+                userData.getDailyNote()
+            }.getOrElse {
+                logger.error { "用户$user 获取便笺数据失败, 已停止推送。原因: ${it.message}" }
+                return
+            }
+
+            val time = dailyNote.resinRecoveryTime - 1800
+
+            repeat++
+            if (time >= 0) delay(time * 1000)
+            else {
+                delay(156 * 8 * 60 * 1000)
+                continue
+            }
+
+            val pushSubject = userSettings[user.id]?.pushSubject ?: throw IllegalStateException("无法找到用户设置！")
+            getSubjectFromBots(pushSubject)?.sendMessage(
+                buildMessageChain {
+                    +At(user)
+                    +"您的树脂还有半小时补满, 请及时上线清理哦! "
+                })
         }
-
-        val time = dailyNote.resinRecoveryTime - 1800
-
-        if (time >= 0) delay(time * 1000)
-        else delay(156 * 8 * 60 * 1000)
-
-        val pushSubject = userSettings[user.id]?.pushSubject ?: throw IllegalStateException("无法找到用户设置！")
-        getSubjectFromBots(pushSubject)?.sendMessage(
-            buildMessageChain {
-                +At(user)
-                +"您的树脂还有半小时补满, 请及时上线清理哦! "
-            })
-
-        main()
     }
 }
